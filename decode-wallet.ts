9c369aa6a365a8d7327871738a8abef2aaa3cb70
Raymond Turla <rayturla21@yahoo.com>
2024-02-14 23:41:22 +0800

    Remove decode wallet

--- decode-wallet.ts
+++
@@ -1,5 +0,0 @@
-import base58 from "bs58";
-import { Keypair } from "@solana/web3.js";
-import wallet from './dev-wallet.json'
-
-console.log(base58.encode(Keypair.fromSecretKey(new Uint8Array(wallet)).secretKey))
