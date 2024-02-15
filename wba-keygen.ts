3f5f4bd324f4909ae0f374b94d517e388906efa7
Raymond Turla <rayturla21@yahoo.com>
2024-02-16 04:38:34 +0800

    Add generate keypair from secret key and refined logs

--- wba-keygen.ts
+++ wba-keygen.ts
@@ -1,11 +1,19 @@
 import {config} from 'dotenv';
 import base58 from 'bs58';
 import fs, { existsSync } from 'fs';
+import { saveWalletToFile } from './utils';
+import { Keypair } from '@solana/web3.js';
 config();
 
 try {
+  // Decode a base 58 secret key value to Uint8Array array
+  const decoded = base58.decode(process.env.WBA_SECRET_KEY ?? "");
+
+  // Generate wallet from decoded base58 secret key.
+  const kp = Keypair.fromSecretKey(new Uint8Array(decoded))
+
   // Save generated secret key to file.
   saveWalletToFile("wba-wallet.json", `[${kp.secretKey}]`);
 } catch(e: any) {
-  console.log(`Error on writing file: ${e.message}`);
+  console.log(`Error on generating wba wallet: ${e.message}. Check WBA_SECRET_KEY in your .env`);
 }
